91455679748578a1da5f11be33eea9c3f79b6159
alekseyl <leshchuk@gmail.com>
2017-02-28 07:14:31 -0800

    remove development test from assets

--- Gemfile.d/assets.rb
+++ Gemfile.d/assets.rb
@@ -1,4 +1,4 @@
-group :development, :test do
+
   gem 'dress_code', '1.0.2', github: "colleenpalmer/dress_code"
     gem 'colored', '1.2', require: false
     gem 'mustache', '1.0.3', require: false
@@ -10,4 +10,3 @@ group :development, :test do
     gem 'coffee-script-source', '1.6.2' #pinned so everyone's compiled output matches
   gem 'yard', '0.9.5'
   gem 'yard-appendix', '0.1.8'
-end
