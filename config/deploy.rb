725ec5de064ec22a6b66415a30d086153b0c3a7b
alekseyl <leshchuk@gmail.com>
2017-03-11 16:01:02 -0800

    local deploy+

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,11 +19,9 @@ set :repository, 'git@github.com:alekseyl/canvas-lms.git'
 set :branch, 'stable'
 set :user, 'ubuntu'
 
-task local_run do
+task :local_run do
   set :domain, 'localhost'
 end
-
-
 
 # Optional settings:
 #   set :user, 'foobar'          # Username in the server to SSH to.
